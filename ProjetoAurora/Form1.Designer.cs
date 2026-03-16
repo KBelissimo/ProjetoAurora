96f8f80204aefa2ef8f50ecb1eacc8232bd4e5a7
KBelissimo <k.belissimo@unesp.br>
2026-03-16 20:31:21 -0300

    Milestone 2

--- ProjetoAurora/Form1.Designer.cs
+++ ProjetoAurora/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            numPotencia = new NumericUpDown();
+            button1 = new Button();
+            lblv = new Label();
+            ((System.ComponentModel.ISupportInitialize)numPotencia).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -42,14 +46,44 @@
             label1.Text = "SISTEMA DE BORDO AURORA";
             label1.TextAlign = ContentAlignment.BottomLeft;
             // 
+            // numPotencia
+            // 
+            numPotencia.Location = new Point(303, 136);
+            numPotencia.Name = "numPotencia";
+            numPotencia.Size = new Size(120, 23);
+            numPotencia.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(429, 136);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "ACELERAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // lblv
+            // 
+            lblv.AutoSize = true;
+            lblv.Location = new Point(366, 238);
+            lblv.Name = "lblv";
+            lblv.Size = new Size(0, 15);
+            lblv.TabIndex = 3;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblv);
+            Controls.Add(button1);
+            Controls.Add(numPotencia);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numPotencia).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +91,8 @@
         #endregion
 
         private Label label1;
+        private NumericUpDown numPotencia;
+        private Button button1;
+        private Label lblv;
     }
 }
--- ProjetoAurora/Form1.cs
+++ ProjetoAurora/Form1.cs
@@ -2,9 +2,21 @@ namespace ProjetoAurora
 {
     public partial class Form1 : Form
     {
+        int velocidadeTotal = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            velocidadeTotal += (int) numPotencia.Value;
+            lblv.Text=velocidadeTotal.ToString();
         }
     }
 }
