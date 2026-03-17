ae87930819f44d480269a1e6a78569f5fbff65f7
Mathinho-tester <matheus.deanin@unesp.br>
2026-03-16 21:16:29 -0300

    Milestone 3

--- ProjetoAurora/Form1.Designer.cs
+++ ProjetoAurora/Form1.Designer.cs
@@ -2,9 +2,6 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
@@ -28,7 +25,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            trackOxigenio = new TrackBar();
+            lblPor = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackOxigenio).BeginInit();
             SuspendLayout();
+            // 
+            // trackOxigenio
+            // 
+            trackOxigenio.LargeChange = 1;
+            trackOxigenio.Location = new Point(315, 258);
+            trackOxigenio.Maximum = 100;
+            trackOxigenio.Name = "trackOxigenio";
+            trackOxigenio.Size = new Size(104, 45);
+            trackOxigenio.TabIndex = 0;
+            trackOxigenio.Value = 10;
+            trackOxigenio.Scroll += trackOxigenio_Scroll;
+            // 
+            // lblPor
+            // 
+            lblPor.AutoSize = true;
+            lblPor.ForeColor = SystemColors.ButtonHighlight;
+            lblPor.Location = new Point(443, 258);
+            lblPor.Name = "lblPor";
+            lblPor.Size = new Size(0, 15);
+            lblPor.TabIndex = 1;
+            lblPor.Click += label1_Click;
             // 
             // Form1
             // 
@@ -36,11 +57,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Navy;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblPor);
+            Controls.Add(trackOxigenio);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackOxigenio).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TrackBar trackOxigenio;
+        private Label lblPor;
     }
 }
--- ProjetoAurora/Form1.cs
+++ ProjetoAurora/Form1.cs
@@ -2,9 +2,22 @@ namespace ProjetoAurora
 {
     public partial class Form1 : Form
     {
+        int oxigenio = 100;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackOxigenio_Scroll(object sender, EventArgs e)
+        {
+                oxigenio = (int)trackOxigenio.Value;
+                lblPor.Text = oxigenio.ToString() + "%";
         }
     }
 }
